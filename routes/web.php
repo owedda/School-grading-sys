ddc2126356df1474e082351e32281e88b4617222
OvidijusRapalis <orapalis@gmail.com>
2022-11-12 22:58:54 +0200

    web routing fixed

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,29 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserLessonController;
 use Illuminate\Support\Facades\Route;
 
+Route::controller(UserController::class)
+    ->prefix('users')
+    ->name('users.')
+    ->group(function () {
+    Route::get('', 'index')->name('index');
+    Route::get('create', 'create')->name('create');
+    Route::post('', 'store')->name('store');
+    Route::delete('{id}', 'destroy')->name('destroy');
+});
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::controller(LessonController::class)
+    ->prefix('lessons')
+    ->name('lessons.')
+    ->group(function () {
+        Route::get('', 'index')->name('index');
+    });
 
-Route::resource('users', UserController::class);
-//Route::get('/', [UserController::class, 'index']);
-//Route::get('/edit/{id}', [UserController::class, 'edit']);
-//Route::get('/show/{id}', [UserController::class, 'show']);
-//Route::get('/create', [UserController::class, 'create']);
-//Route::post('/store', [UserController::class, 'store']);
-//Route::post('/update/{id}', [UserController::class, 'update']);
-//Route::delete('/{id}', [UserController::class, 'destroy']);
+Route::controller(UserLessonController::class)
+    ->prefix('userLessons')
+    ->name('userLessons.')
+    ->group(function () {
+        Route::get('', 'index')->name('index');
+        Route::post('', 'store')->name('store');
+        Route::delete('{id}', 'destroy')->name('destroy');
+    });
 
-Route::resource('lessons', LessonController::class);
-Route::resource('userLessons', UserLessonController::class);
