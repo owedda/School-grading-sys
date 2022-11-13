5a30e8191347a6077a3d995d487369eeb19df58a
OvidijusRapalis <orapalis@gmail.com>
2022-11-13 23:00:20 +0200

    removed unused element

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -6,38 +6,5 @@ use Illuminate\Http\Request;
 
 class EvaluationController extends Controller
 {
-    public function index()
-    {
-        //
-    }
 
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show($id)
-    {
-        //
-    }
-
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
-    }
 }
