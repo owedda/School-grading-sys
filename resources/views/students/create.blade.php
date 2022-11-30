096439e5989060d509de0af5b508a44d4c3fa9eb
OvidijusRapalis <orapalis@gmail.com>
2022-11-30 14:00:04 +0200

    removed unused language elements, changed to just hardcoded text

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -3,71 +3,68 @@
 
     <div class="card">
         <div class="card-header">
-            {{ __('global.create') }} {{ __('cruds.user.title_singular') }}
+            Register student
         </div>
 
         <div class="card-body">
             <form method="POST" action="{{ route("users.store") }}" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
-                    <label class="required" for="username">{{ __('cruds.user.fields.username') }}</label>
-                    <input class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}" type="text" name="username" id="username" value="{{ old('username', '') }}" required>
+                    <label class="required" for="username">Username</label>
+                    <input class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}" type="text" name="username" id="username" value="" required>
                     @if($errors->has('username'))
                         <div class="invalid-feedback">
                             {{ $errors->first('username') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ __('cruds.user.fields.name_helper') }}</span>
+                    <span class="help-block"></span>
                 </div>
                 <div class="form-group">
-                    <label class="required" for="name">{{ __('cruds.user.fields.name') }}</label>
+                    <label class="required" for="name">Name</label>
                     <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
                     @if($errors->has('name'))
                         <div class="invalid-feedback">
                             {{ $errors->first('name') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ __('cruds.user.fields.name_helper') }}</span>
+                    <span class="help-block"></span>
                 </div>
                 <div class="form-group">
-                    <label class="required" for="last-name">{{ __('cruds.user.fields.last_name') }}</label>
+                    <label class="required" for="last-name">Last name</label>
                     <input class="form-control {{ $errors->has('last-name') ? 'is-invalid' : '' }}" type="text" name="last-name" id="last-name" value="{{ old('last-name', '') }}" required>
                     @if($errors->has('last-name'))
                         <div class="invalid-feedback">
                             {{ $errors->first('last-name') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ __('cruds.user.fields.name_helper') }}</span>
+                    <span class="help-block"></span>
                 </div>
                 <div class="form-group">
-                    <label class="required" for="email">{{ trans('cruds.user.fields.email') }}</label>
+                    <label class="required" for="email">Email</label>
                     <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" id="email" value="{{ old('email') }}" required>
                     @if($errors->has('email'))
                         <div class="invalid-feedback">
                             {{ $errors->first('email') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ trans('cruds.user.fields.email_helper') }}</span>
+                    <span class="help-block"></span>
                 </div>
                 <div class="form-group">
-                    <label class="required" for="password">{{ trans('cruds.user.fields.password') }}</label>
+                    <label class="required" for="password">Password</label>
                     <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password" required>
                     @if($errors->has('password'))
                         <div class="invalid-feedback">
                             {{ $errors->first('password') }}
                         </div>
                     @endif
-                    <span class="help-block">{{ trans('cruds.user.fields.password_helper') }}</span>
+                    <span class="help-block"></span>
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-danger" type="submit">
-                        {{ trans('global.save') }}
+                    <button class="btn btn-success" type="submit">
+                        Save
                     </button>
                 </div>
             </form>
         </div>
     </div>
-
-
-
 @endsection
